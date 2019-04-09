2beeb5b2e7712ebad866a3a31859e1b4a7545a15
Raven Robertson <ravewillow6383@gmail.com>
2019-04-09 12:50:26 -0700

    moved functions into objects as methods, finished code for all locations

--- js/app.js
+++ js/app.js
@@ -5,81 +5,200 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 var pike = {
   minCustomers: 23,
   maxCustomer: 65,
-  avgCookiesSold:6.3,
-  id: 'ul1stAndPike',
+  avgCookiesSold: 6.3,
+  id: 'ul1st-and-pike',
   cookiesPerHour:[],
 
   totalCookies: 0,
   hourlyCustomer: function (min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   },
-};
-
-for(var i=0; i<hours.length; i++) {
-  pike.cookiesPerHour.push(Math.ceil(pike.hourlyCustomer(pike.minCustomers,pike.maxCustomer)*pike.avgCookiesSold));
-}
-
-pike.dailyTotal = function(){
-  for(var i=0; i<this.cookiesPerHour.length; i++){
-    pike.totalCookies += this.cookiesPerHour[i];
-  }
-};
-
-pike.render= function(){
-  var referenceUl = document.getElementById(this.id);
-  for(var i =0; i<this.cookiesPerHour.length; i++){
-
-    var liElement = document.createElement('li');
-
-    liElement.textContent = `at ${hours[i]} we sold ${this.cookiesPerHour[i]}.`;
-
+  hourlyTotals: function() {
+    for ( var i=0; i<hours.length; i++) {
+      this.cookiesPerHour.push(Math.ceil(this.hourlyCustomer(this.minCustomers, this.maxCustomer)*this.avgCookiesSold));
+    }
+  },
+  dailyTotal: function(){
+    for(var i=0; i<this.cookiesPerHour.length; i++){
+      this.totalCookies += this.cookiesPerHour[i];
+    }
+  },
+  dailyStats: function(){
+    this.hourlyTotals();
+    this.dailyTotal();
+    this.render();
+  },
+  render: function(){
+    var referenceUl = document.getElementById(this.id);
+    for(var i = 0; i<this.cookiesPerHour.length; i++){
+      var liElement = document.createElement('li');
+      liElement.textContent = `at ${hours[i]} we sold ${this.cookiesPerHour[i]}.`;
+      referenceUl.append(liElement);
+    }
+    liElement.textContent = `We sold a total of ${this.totalCookies}`;
     referenceUl.append(liElement);
   }
-  liElement.textContent = `We sold a total of ${pike.totalCookies}`;
-  referenceUl.append(liElement);
 };
 
-pike.dailyTotal();
-pike.render();
+pike.dailyStats();
 
 var seaTac = {
   minCustomers: 3,
   maxCustomer: 24,
   avgCookiesSold: 1.2,
-  id: 'ulSeaTac',
+  id: 'ul-SeaTac',
   cookiesPerHour:[],
 
   totalCookies: 0,
   hourlyCustomer: function (min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   },
-};
-
-for ( i=0; i<hours.length; i++) {
-  seaTac.cookiesPerHour.push(Math.ceil(seaTac.hourlyCustomer(seaTac.minCustomers, seaTac.maxCustomer)*seaTac.avgCookiesSold));
-}
-
-seaTac.dailyTotal = function(){
-  for(var i=0; i<this.cookiesPerHour.length; i++){
-    seaTac.totalCookies += this.cookiesPerHour[i];
-  }
-};
-
-seaTac.render= function(){
-    // Vinicio - ok so line 70 is the one that's not working. As soon as this line
-    // runs, we get a null value.
-  var referenceUl = document.getElementById(this.id);
-  for(var i = 0; i<this.cookiesPerHour.length; i++){
-
-    var liElement = document.createElement('li');
-
-    liElement.textContent = `at ${hours[i]} we sold ${this.cookiesPerHour[i]}.`;
-
+  hourlyTotals: function() {
+    for (var i=0; i<hours.length; i++) {
+      this.cookiesPerHour.push(Math.ceil(this.hourlyCustomer(this.minCustomers, this.maxCustomer)*this.avgCookiesSold));
+    }
+  },
+  dailyTotal: function(){
+    for(var i=0; i<this.cookiesPerHour.length; i++){
+      this.totalCookies += this.cookiesPerHour[i];
+    }
+  },
+  dailyStats: function(){
+    this.hourlyTotals();
+    this.dailyTotal();
+    this.render();
+  },
+  render: function(){
+    var referenceUl = document.getElementById(this.id);
+    for(var i = 0; i<this.cookiesPerHour.length; i++){
+      var liElement = document.createElement('li');
+      liElement.textContent = `at ${hours[i]} we sold ${this.cookiesPerHour[i]}.`;
+      referenceUl.append(liElement);
+    }
+    liElement.textContent = `We sold a total of ${this.totalCookies}`;
     referenceUl.append(liElement);
   }
-  liElement.textContent = `We sold a total of ${seaTac.totalCookies}`;
-  referenceUl.append(liElement);
 };
 
-seaTac.dailyTotal();
-seaTac.render();
+seaTac.dailyStats();
+
+var seaCenter = {
+  minCustomers: 11,
+  maxCustomer: 38,
+  avgCookiesSold: 3.7,
+  id: 'ul-seattle-center',
+  cookiesPerHour:[],
+
+  totalCookies: 0,
+  hourlyCustomer: function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  },
+  hourlyTotals: function() {
+    for (var i=0; i<hours.length; i++) {
+      this.cookiesPerHour.push(Math.ceil(this.hourlyCustomer(this.minCustomers, this.maxCustomer)*this.avgCookiesSold));
+    }
+  },
+  dailyTotal: function(){
+    for(var i=0; i<this.cookiesPerHour.length; i++){
+      this.totalCookies += this.cookiesPerHour[i];
+    }
+  },
+  dailyStats: function(){
+    this.hourlyTotals();
+    this.dailyTotal();
+    this.render();
+  },
+  render: function(){
+    var referenceUl = document.getElementById(this.id);
+    for(var i = 0; i<this.cookiesPerHour.length; i++){
+      var liElement = document.createElement('li');
+      liElement.textContent = `at ${hours[i]} we sold ${this.cookiesPerHour[i]}.`;
+      referenceUl.append(liElement);
+    }
+    liElement.textContent = `We sold a total of ${this.totalCookies}`;
+    referenceUl.append(liElement);
+  }
+};
+
+seaCenter.dailyStats();
+
+var capHill = {
+  minCustomers: 20,
+  maxCustomer: 38,
+  avgCookiesSold: 2.3,
+  id: 'ul-capitol-hill',
+  cookiesPerHour:[],
+
+  totalCookies: 0,
+  hourlyCustomer: function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  },
+  hourlyTotals: function() {
+    for (var i=0; i<hours.length; i++) {
+      this.cookiesPerHour.push(Math.ceil(this.hourlyCustomer(this.minCustomers, this.maxCustomer)*this.avgCookiesSold));
+    }
+  },
+  dailyTotal: function(){
+    for(var i=0; i<this.cookiesPerHour.length; i++){
+      this.totalCookies += this.cookiesPerHour[i];
+    }
+  },
+  dailyStats: function(){
+    this.hourlyTotals();
+    this.dailyTotal();
+    this.render();
+  },
+  render: function(){
+    var referenceUl = document.getElementById(this.id);
+    for(var i = 0; i<this.cookiesPerHour.length; i++){
+      var liElement = document.createElement('li');
+      liElement.textContent = `at ${hours[i]} we sold ${this.cookiesPerHour[i]}.`;
+      referenceUl.append(liElement);
+    }
+    liElement.textContent = `We sold a total of ${this.totalCookies}`;
+    referenceUl.append(liElement);
+  }
+};
+
+capHill.dailyStats();
+
+var alki = {
+  minCustomers: 2,
+  maxCustomer: 16,
+  avgCookiesSold: 4.6,
+  id: 'ul-alki',
+  cookiesPerHour:[],
+
+  totalCookies: 0,
+  hourlyCustomer: function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  },
+  hourlyTotals: function() {
+    for (var i=0; i<hours.length; i++) {
+      this.cookiesPerHour.push(Math.ceil(this.hourlyCustomer(this.minCustomers, this.maxCustomer)*this.avgCookiesSold));
+    }
+  },
+  dailyTotal: function(){
+    for(var i=0; i<this.cookiesPerHour.length; i++){
+      this.totalCookies += this.cookiesPerHour[i];
+    }
+  },
+  dailyStats: function(){
+    this.hourlyTotals();
+    this.dailyTotal();
+    this.render();
+  },
+  render: function(){
+    var referenceUl = document.getElementById(this.id);
+    for(var i = 0; i<this.cookiesPerHour.length; i++){
+      var liElement = document.createElement('li');
+      liElement.textContent = `at ${hours[i]} we sold ${this.cookiesPerHour[i]}.`;
+      referenceUl.append(liElement);
+    }
+    liElement.textContent = `We sold a total of ${this.totalCookies}`;
+    referenceUl.append(liElement);
+  }
+};
+
+alki.dailyStats();
+
